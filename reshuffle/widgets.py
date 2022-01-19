34b0b382a1c7539dd1c1e6a1bca3fea19a73784c
r4nd0lph <r11345roma@yandex.ru>
2022-01-19 22:35:44 +0300

    add comment to LatexInput (widgets.py)

--- reshuffle/widgets.py
+++ reshuffle/widgets.py
@@ -2,5 +2,6 @@ from django.forms.widgets import Input
 
 
 class LatexInput(Input):
+    """ Custom widget for LaTeX format text input """
     input_type = 'text'
     template_name = 'reshuffle/widgets/latex_widget.html'
